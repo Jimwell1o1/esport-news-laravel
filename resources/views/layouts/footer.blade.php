98c3cffe7714fe1583c52c29717db5eba0f7a245
Jimwell1o1 <82427374+Jimwell1o1@users.noreply.github.com>
2021-04-22 23:01:29 +0800

    Update footer.blade.php

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,1 +1,83 @@
-<div>This is footer</div>
+
+
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-info">
+            <h3>ESPORTS</h3>
+            <p>This is a forum where people can get up to date on the latest developments in the esports industry. We'd like to keep you up to date with all that is going on in the Esports industry. </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links">
+            <h4>Useful Links</h4>
+            <ul>
+              <li><i class="bi bi-chevron-right"></i> <a href="index.html">Home</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="news.html">News</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="about.html">About us</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="portfolio.html">Gallery</a></li>
+              <li><i class="bi bi-chevron-right"></i> <a href="contact.html">Contact Us</a></li>
+            </ul>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-contact">
+            <h4>Contact Us</h4>
+            <p>
+              12-B Alcalde Jose<br>
+              Pasig, 1600 <br>
+              Metro Manila<br>
+              <strong>Phone:</strong> (02) 8628 1013<br>
+              <strong>Email:</strong> plpasig@plpasig.edu.ph<br>
+            </p>
+
+            <div class="social-links">
+              <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+              <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+              <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+              <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+              <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+            </div>
+
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-newsletter">
+            <h4>Our Newsletter</h4>
+            <p>Subscribe to our Newsletter if you want to get updated for hot updates and news. Just type in your email and hit that Subscribe button! </p>
+            <form action="" method="post">
+              <input type="email" name="email"><input type="submit" value="Subscribe">
+            </form>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container">
+      <div class="copyright">
+        &copy; Copyright <strong>CS - 3A </strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        Group 8 <a href="#">ESPORTS</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i class="bi bi-arrow-up-short"></i></a>
+  <!-- Uncomment below i you want to use a preloader -->
+  <!-- <div id="preloader"></div> -->
+
+  <!-- Vendor JS Files -->
+  <script src="{{ asset('/vendor/aos/aos.js') }}"></script>
+  <script src="{{ asset('/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+  <script src="{{ asset('/vendor/glightbox/js/glightbox.min.js') }}"></script>
+  <script src="{{ asset('/vendor/isotope-layout/isotope.pkgd.min.js') }}"></script>
+  <script src="{{ asset('/vendor/php-email-form/validate.js') }}"></script>
+  <script src="{{ asset('/vendor/purecounter/purecounter.js') }}"></script>
+  <script src="{{ asset('/vendor/swiper/swiper-bundle.min.js') }}"></script>
+  <script src="{{ asset('/vendor/waypoints/noframework.waypoints.js') }}"></script>
+
+  <!-- Template Main JS File -->
+  <script src="{{ asset('/js/main.js') }}"></script>
