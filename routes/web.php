457cfa8c79bdb9a1801e3f7a6a4213af0208b017
Jimwell1o1 <82427374+Jimwell1o1@users.noreply.github.com>
2021-04-22 23:04:20 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,4 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
 Route::get('/', [App\Http\Controllers\myController::class, 'index']);
+Route::get('index', [App\Http\Controllers\myController::class, 'index']);
+Route::get('about', [App\Http\Controllers\myController::class, 'about']);
+Route::get('news', [App\Http\Controllers\myController::class, 'news']);
+Route::get('gallery', [App\Http\Controllers\myController::class, 'gallery']);
+Route::get('team', [App\Http\Controllers\myController::class, 'team']);
+Route::get('contact', [App\Http\Controllers\myController::class, 'contact']);
+
