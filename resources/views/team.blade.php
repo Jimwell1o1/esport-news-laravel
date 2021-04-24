3e052bc16238a5a21d6bf5e1b86425d51718d4ac
JcPac <paclibar.johncarlo.ccs@gmail.com>
2021-04-24 15:14:04 +0800

    Update team.blade.php

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -1,6 +1,19 @@
-@extends('layouts.master')
+@extends('layouts.layout')
 
-
-@section('content')
-<div>This is team</div>
+@section('headercontent')
+<header id="header" class="fixed-top d-flex align-items-center">
 @endsection
+
+@section('nav')
+<nav id="navbar" class="navbar">
+            <ul>
+              <li><a class="nav-link scrollto" href="{{ url('index') }}">Home</a></li>
+              <li><a class="nav-link scrollto" href="{{ url('news') }}">News</a></li>
+              <li><a class="nav-link scrollto" href="{{ url('about') }}">About</a></li>
+              <li><a class="nav-link scrollto" href="{{ url('gallery') }}">Gallery</a></li>
+              <li><a class="nav-link scrollto active" href="{{ url('team') }}">Team</a></li>
+              </li>
+              <li><a class="nav-link scrollto" href="{{ url('contact') }}">Contact</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+          </nav><!-- .navbar -->
