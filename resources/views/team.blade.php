7e0a1c99b1f3e75f2196817cc3124cbcdc6e03ba
JcPac <paclibar.johncarlo.ccs@gmail.com>
2021-04-24 15:52:16 +0800

    Update team.blade.php

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -86,3 +86,109 @@
           </div>
         </div>
       </div>
+
+
+      <div class="col-lg-3 col-md-6">
+        <div class="member" data-aos="fade-up" data-aos-delay="400">
+          <img src="{{ asset('/img/team/team-4.jpg') }}" class="img-fluid" alt="">
+          <div class="member-info">
+            <div class="member-info-content">
+              <h4>John Carlo Paclibar</h4>
+              <span>Editor in Chief</span>
+              <div class="social">
+                <a href=""><i class="bi bi-twitter"></i></a>
+                <a href=""><i class="bi bi-facebook"></i></a>
+                <a href=""><i class="bi bi-instagram"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+</section><!-- End Team Section -->
+
+<!-- ======= Testimonials Section ======= -->
+<section id="testimonials" class="section-bg">
+  <div class="container" data-aos="fade-up">
+
+    <header class="section-header">
+      <h3>Testimonials</h3>
+    </header>
+
+    <div class="testimonials-slider swiper-container" data-aos="fade-up" data-aos-delay="100">
+      <div class="swiper-wrapper">
+
+        <div class="swiper-slide">
+          <div class="testimonial-item">
+            <img src="{{ asset('/img/testimonials/testimonial-1.jpg') }}" class="testimonial-img" alt="">
+            <h3>AkoSi Dogie</h3>
+            <h4>Youtuber Gamer &amp; Influencer</h4>
+            <p>
+              <img src="{{ asset('/img/quote-sign-left.png') }}" class="quote-sign-left" alt="">
+              The team that keeps winning is not the most talented but the most hard-working.
+              <img src="{{ asset('/img/quote-sign-right.png') }}" class="quote-sign-right" alt="">
+            </p>
+          </div>
+        </div><!-- End testimonial item -->
+
+        <div class="swiper-slide">
+          <div class="testimonial-item">
+            <img src="{{ asset('/img/testimonials/testimonial-2.jpg') }}" class="testimonial-img" alt="">
+            <h3>Alodia Gosiengfiao</h3>
+            <h4>Youtuber Gamer &amp; Model</h4>
+            <p>
+              <img src="{{ asset('/img/quote-sign-left.png') }}" class="quote-sign-left" alt="">
+              Gamers always believe that an epic win is possible and that it's always worth trying, and trying now. Gamers don't sit around.
+              <img src="{{ asset('/img/quote-sign-right.png') }}" class="quote-sign-right" alt="">
+            </p>
+          </div>
+        </div><!-- End testimonial item -->
+
+        <div class="swiper-slide">
+          <div class="testimonial-item">
+            <img src="{{ asset('/img/testimonials/testimonial-3.jpg') }}" class="testimonial-img" alt="">
+            <h3>PewDiePie</h3>
+            <h4>Youtuber Gamer &amp; Comedian</h4>
+            <p>
+              <img src="{{ asset('/img/quote-sign-left.png') }}" class="quote-sign-left" alt="">
+              The one point gamers all hate is the point where they have to put the controller down.
+              <img src="{{ asset('/img/quote-sign-right.png') }}" class="quote-sign-right" alt="">
+            </p>
+          </div>
+        </div><!-- End testimonial item -->
+
+        <div class="swiper-slide">
+          <div class="testimonial-item">
+            <img src="{{ asset('/img/testimonials/testimonial-4.jpg') }}" class="testimonial-img" alt="">
+            <h3>Ghost Wrecker</h3>
+            <h4>Streamer &amp; Gamer</h4>
+            <p>
+              <img src="{{ asset('/img/quote-sign-left.png') }}" class="quote-sign-left" alt="">
+              The more people who come forward and talk about how much they love gaming, how much they talk about individuality and diversity, the more gamers of color that come out and gay gamers that come out and everybody talking about what they love - that's what the community has in common: a love of gaming.
+              <img src="{{ asset('/img/quote-sign-right.png') }}" class="quote-sign-right" alt="">
+            </p>
+          </div>
+        </div><!-- End testimonial item -->
+
+        <div class="swiper-slide">
+          <div class="testimonial-item">
+            <img src="{{ asset('/img/testimonials/testimonial-5.jpg') }}" class="testimonial-img" alt="">
+            <h3>Faker</h3>
+            <h4>Pro Player &amp; Gamer</h4>
+            <p>
+              <img src="{{ asset('/img/quote-sign-left.png') }}" class="quote-sign-left" alt="">
+              At their highest level, all competitive disciplines become vehicles for the expression of one's human potential.
+              <img src="{{ asset('/img/quote-sign-right.png') }}" class="quote-sign-right" alt="">
+            </p>
+          </div>
+        </div><!-- End testimonial item -->
+
+      </div>
+      <div class="swiper-pagination"></div>
+    </div>
+
+  </div>
+</section><!-- End Testimonials Section -->
