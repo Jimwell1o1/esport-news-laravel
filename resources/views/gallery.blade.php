bb02f423ac900157696e4a3e2c8b9f78a514f92a
Carlo <carloraymundoo@gmail.com>
2021-04-24 19:32:06 +0800

    Update gallery.blade.php

--- resources/views/gallery.blade.php
+++ resources/views/gallery.blade.php
@@ -125,3 +125,18 @@
               </div>
             </div>
           </div>
+
+          <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+            <div class="portfolio-wrap">
+              <figure>
+                <img src="{{ asset('/img/portfolio/app2.jpg') }}" class="img-fluid" alt="">
+                <a href="{{ asset('/img/portfolio/app2.jpg') }}" class="link-preview portfolio-lightbox" data-gallery="portfolioGallery" title="App 2"><i class="bi bi-plus"></i></a>
+                <a href="portfolio-details.html" class="link-details" title="More Details"><i class="bi bi-link"></i></a>
+              </figure>
+  
+              <div class="portfolio-info">
+                <h4><a href="portfolio-details.html">Play</a></h4>
+                <p>In-game</p>
+              </div>
+            </div>
+          </div>
