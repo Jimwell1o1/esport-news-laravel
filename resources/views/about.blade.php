bbf8c35471f5a3c2c06eb38f8b65a9492777d5d7
Aaron <82888870+Aaronzxc27@users.noreply.github.com>
2021-04-24 17:39:01 +0800

    Update about.blade.php

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,20 @@
-@extends('layouts.master')
-jimwell
+@extends('layouts.layout')
 
-@section('content')
-<div>This is about</div>
+@section('headercontent')
+<header id="header" class="fixed-top d-flex align-items-center">
+@endsection
+
+@section('nav')
+<nav id="navbar" class="navbar">
+            <ul>
+              <li><a class="nav-link scrollto" href="{{ url('index') }}">Home</a></li>
+              <li><a class="nav-link scrollto" href="{{ url('news') }}">News</a></li>
+              <li><a class="nav-link scrollto active" href="{{ url('about') }}">About</a></li>
+              <li><a class="nav-link scrollto " href="{{ url('gallery') }}">Gallery</a></li>
+              <li><a class="nav-link scrollto" href="{{ url('team') }}">Team</a></li>
+              </li>
+              <li><a class="nav-link scrollto" href="{{ url('contact') }}">Contact</a></li>
+            </ul>
+            <i class="bi bi-list mobile-nav-toggle"></i>
+          </nav><!-- .navbar -->
 @endsection
