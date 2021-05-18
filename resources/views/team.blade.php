d95a187c8dc708f1b11842f54f64ea0aeb0b3ef4
Jimwell1o1 <82427374+Jimwell1o1@users.noreply.github.com>
2021-05-18 22:44:24 +0800

    Update href's

--- resources/views/team.blade.php
+++ resources/views/team.blade.php
@@ -9,7 +9,7 @@
             <ul>
               <li><a class="nav-link scrollto" href="{{ url('index') }}">Home</a></li>
               <li><a class="nav-link scrollto" href="{{ url('news') }}">News</a></li>
-              <li><a class="nav-link scrollto" href="{{ url('contact') }}">Blog</a></li>
+              <li><a class="nav-link scrollto" href="{{ url('blog') }}">Blog</a></li>
               <li><a class="nav-link scrollto" href="{{ url('about') }}">About</a></li>
               <li><a class="nav-link scrollto" href="{{ url('gallery') }}">Gallery</a></li>
               <li><a class="nav-link scrollto active" href="{{ url('team') }}">Team</a></li>
