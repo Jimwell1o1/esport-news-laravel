129ab001d154e130a25f138c9944ed5b90b4cfab
JcPac <paclibar.johncarlo.ccs@gmail.com>
2021-05-24 20:59:23 +0800

    Update create.blade.php

--- resources/views/blog/create.blade.php
+++ resources/views/blog/create.blade.php
@@ -15,31 +15,18 @@
               <li><a class="nav-link scrollto" href="{{ url('gallery') }}">Gallery</a></li>
               <li><a class="nav-link scrollto" href="{{ url('team') }}">Team</a></li>
               <li><a class="nav-link scrollto" href="{{ url('contact') }}">Contact</a></li>
-              <li><a class="btn btn-primary sign-up-btn" href="{{ url('login') }}">Log In</a></li>
+          
+            
+              @include('layouts.navbar')
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
+
           </nav><!-- .navbar -->
 @endsection
 
 
 @section('content')
 
-
-
-    <!-- ======= Breadcrumbs Section ======= -->
-    <section class="breadcrumbs">
-        <div class="container">
-  
-          <div class="d-flex justify-content-between align-items-center">
-            <h2>Create a Post</h2>
-            <ol>
-              <li><a href="{{ url('blog') }}">Blog</a></li>
-              <li>Create Post</li>
-            </ol>
-          </div>
-  
-        </div>
-      </section><!-- End Breadcrumbs Section -->
 
 
       @if ($errors->any())
@@ -53,21 +40,26 @@
           
       @endif
   
-      <form 
+  <!--    OLD FORM FOR POST
+          <form 
             action="/blog"
             method="POST"
             enctype="multipart/form-data">
             @csrf
 
-            <input 
+            <h5>TITLE</h5>
+            <input class="form-control" 
                 type="text" 
                 name="title" 
-                placeholder="Title...">
+                placeholder="Post Title.....">
                 <br>
-            <textarea 
+
+            <h5>DESCRIPTION</h5>
+            <textarea  class="form-control
                 name="description" 
-                placeholder="description..."></textarea>
+                placeholder="Post Description....."></textarea>
                 <br>
+
             <label>
               <span>
                 Select a file
@@ -76,10 +68,85 @@
                   type="file"
                   name="image">
             </label>
-            <br>
-            <button
+            <br><br>
+
+            <button class="btn btn-primary"
                 type="submit">
               Submit Post
             </button>
       </form>
+
+    <br><br>-->
+
+
+
+     <!-- ======= Create post ======= -->
+
+     <section class="breadcrumbs">
+        <div class="container">
+          <div class="d-flex justify-content-between align-items-center">
+           
+            <ol>
+              <li><a href="{{ url('blog') }}">Blog</a></li>
+              <li>Create Post</li>
+            </ol>
+          </div>
+  
+        </div>
+      </section>
+
+    <section id="contact" class="section-bg">
+        <div class="container" data-aos="fade-up">
+  
+
+          <div class="section-header justify-content-between">
+            <h3>CREATE POST</h3>
+
+          </div>
+  
+         
+          <div class="form">
+            <form action="forms/contact.php" method="post" role="form" class="php-email-form">
+      
+             
+              <div class="form-group">
+              TITLE
+                <input type="text" 
+                class="form-control" 
+                name="title" 
+                id="subject" 
+                placeholder="Post Title..." required>
+              </div>
+              <div class="form-group">
+              DESCRIPTION
+                <textarea class="form-control" 
+                name="description" 
+                rows="5"
+                placeholder="Post Descriptions..." required></textarea>
+              </div>
+
+              <label>
+              <span>
+                Select a file
+              </span>
+              <input 
+                  type="file"
+                  name="image">
+            </label>
+
+
+              <div class="my-3">
+                <div class="loading">Loading</div>
+                <div class="error-message"></div>
+                <div class="sent-message">Your Post has been sent. Thank you!</div>
+              </div>
+
+              <div class="text-center">
+              <button type="submit">SUBMIT POST</button>
+              </div>
+            </form>
+          </div>
+  
+        </div>
+      </section><!-- End Contact Section -->
 @endsection
