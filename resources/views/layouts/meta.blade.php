bb15e922d0cb874034d163ae9e88c29496aa6017
Jimwell1o1 <82427374+Jimwell1o1@users.noreply.github.com>
2021-05-24 17:16:29 +0800

    Update meta.blade.php

--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -1,11 +1,22 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <meta charset="utf-8">
-  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+<!-- CSRF Token -->
+<meta name="csrf-token" content="{{ csrf_token() }}">
 
-  <title>ESPORTS</title>
-  <meta content="" name="description">
-  <meta content="" name="keywords">
-  
+<title>{{ config('app.name', 'Laravel') }}</title>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}" defer></script>
+
+<!-- Fonts -->
+<link rel="dns-prefetch" href="//fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+<!-- Styles -->
+<link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+
   <!-- Favicons -->
   <link href="{{ asset('/img/favicon.png') }}" rel="icon">
   <link href="{{ asset('/img/apple-touch-icon.png') }}" rel="apple-touch-icon">
