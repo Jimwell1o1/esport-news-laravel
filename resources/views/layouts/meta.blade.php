6ec337b2fc609fba16cb99ea047ce6ffbf48f0cd
JcPac <paclibar.johncarlo.ccs@gmail.com>
2021-05-24 20:59:08 +0800

    Update meta.blade.php

--- resources/views/layouts/meta.blade.php
+++ resources/views/layouts/meta.blade.php
@@ -4,7 +4,7 @@
 <!-- CSRF Token -->
 <meta name="csrf-token" content="{{ csrf_token() }}">
 
-<title>{{ config('Esports', 'Esports') }}</title>
+<title>{{ config('app.name', 'Esports') }}</title>
 
 <!-- Scripts -->
 <script src="{{ asset('js/app.js') }}" defer></script>
